1984b91acc25b01056413fff0672a6f24fa4bfe9
Ryan Howard <133996155+ryan-howard-hc@users.noreply.github.com>
2023-12-13 12:55:05 -0500

    Update 03pacmove.js

--- pacman/src/app/03pacmove.js
+++ pacman/src/app/03pacmove.js
@@ -1,51 +1,18 @@
-// import React from 'react';
-// import { useClient } from 'vitedge/client';
+import { useEffect } from 'react';
 
-// const PacMove = ({ boardData, initialPosition }) => {
-//   useClient();
-
-//   const [pacManPosition, setPacManPosition] = React.useState(initialPosition);
-
-//   React.useEffect(() => {
-//     const handleKeyPress = (event) => {
-//       movePacMan(event.key);
-//     };
-
-//     window.addEventListener('keydown', handleKeyPress);
-
-//     return () => {
-//       window.removeEventListener('keydown', handleKeyPress);
-//     };
-//   }, [boardData, initialPosition]);
-
-//   const movePacMan = (direction) => {
-//     const { x: currentX, y: currentY } = pacManPosition;
-//     let newX = currentX;
-//     let newY = currentY;
-
-//     switch (direction) {
-//       case 'ArrowUp':
-//         newY = currentY - 1;
-//         break;
-//       case 'ArrowDown':
-//         newY = currentY + 1;
-//         break;
-//       case 'ArrowLeft':
-//         newX = currentX - 1;
-//         break;
-//       case 'ArrowRight':
-//         newX = currentX + 1;
-//         break;
-//       default:
-//         return;
-//     }
-
-//     if (boardData[newY] && boardData[newY][newX] !== 'X') {
-//       setPacManPosition({ x: newX, y: newY });
-//     }
-//   };
-
-//   return null; 
-// };
-
-// export default PacMove;
+const PacMove = ({ movePacMan }) => {
+    useEffect(() => {
+        const handleKeyPress = (event) => {
+          movePacMan(event.key);
+        };
+    
+        window.addEventListener('keydown', handleKeyPress);
+    
+        return () => {
+          window.removeEventListener('keydown', handleKeyPress);
+        };
+      }, [movePacMan]);
+    
+      return null; // This component doesn't render anything
+    };
+export default PacMove;
